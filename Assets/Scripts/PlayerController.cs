0a31c97d4fd3e757432d8ee596a90203466ee25c
Brad Slavens <Bradslavens@users.noreply.github.com>
2020-11-05 11:57:27 -0800

    added movement via translate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,47 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Animator _animator;   
+
+    private Rigidbody2D _rigidBody2D;
+    private Animator _animator;   
     [SerializeField] private float _velocity;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    private void Start() {
+        _rigidBody2D  = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+    private void FixedUpdate() {
+        float _moveHorizontal = Input.GetAxis("Horizontal");
+        float _moveVertical = Input.GetAxis("Vertical");
+        Vector3 _movement = new Vector3(_moveHorizontal, _moveVertical, 0);
+        _movement *= Time.deltaTime;
+        _movement *= _velocity;
 
-        if(Input.GetKey(KeyCode.W)) {
-
-            newPosition.y = transform.position.y + 1 * _velocity * Time.deltaTime ;
-            transform.position = newPosition;
-            _animator.SetBool("walkForward", true);
-
-        } else if ( Input.GetKey(KeyCode.S) ){     
-
-            newPosition.y = transform.position.y - 1 * _velocity * Time.deltaTime ;
-            transform.position = newPosition;
-            _animator.SetBool("walkBackward", true);
-
-        } else if ( Input.GetKey(KeyCode.A) ){     
-
-            newPosition.x = transform.position.x - 1 * _velocity * Time.deltaTime ;
-            transform.position = newPosition;
-            _animator.SetBool("walkLeft", true);
-
-        } else if ( Input.GetKey(KeyCode.D) ){     
-
-            newPosition.x = transform.position.x + 1 * _velocity * Time.deltaTime ;
-            transform.position = newPosition;
-            _animator.SetBool("walkRight", true);
-            
-        } else {
-            
-            _animator.SetBool("walkForward", false);
-            _animator.SetBool("walkBackward", false);            
-            _animator.SetBool("walkRight", false);
-            _animator.SetBool("walkLeft", false);
-            
-            _animator.SetBool("standing", true);
-
-        }
+        transform.Translate(_movement);
     }
 }
