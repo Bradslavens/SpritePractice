25f57ab5fb833d8cb25150f93a584e4f0fa19c62
Brad Slavens <Bradslavens@users.noreply.github.com>
2020-11-05 13:23:50 -0800

    added jumping :

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,28 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rigidBody2D;
     private Animator _animator;   
     [SerializeField] private float _velocity;
+    [SerializeField] private float _force;
 
     private void Start() {
         _rigidBody2D  = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
     }
     private void FixedUpdate() {
-        float _moveHorizontal = Input.GetAxis("Horizontal");
-        float _moveVertical = Input.GetAxis("Vertical");
+        float _moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float _moveVertical = Input.GetAxisRaw("Vertical");
+
+        _animator.SetFloat("horizontal", _moveHorizontal);
+        _animator.SetFloat("vertical", _moveVertical);
+
         Vector3 _movement = new Vector3(_moveHorizontal, _moveVertical, 0);
         _movement *= Time.deltaTime;
         _movement *= _velocity;
 
         transform.Translate(_movement);
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            // Jump
+            _rigidBody2D.AddForce(new Vector2(0, _force), ForceMode2D.Impulse);
+        }
     }
 }
